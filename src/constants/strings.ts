818364c861bae15ce6abfa56541271a7ee2b5ffa
thedevme <designtoswiftui@gmail.com>
2026-01-01 16:03:52 -0500

    Organize project folder structure (#4)

---
+++ src/constants/strings.ts
@@ -0,0 +1,10 @@
+export const STRINGS = {
+    app: {
+        name: 'RetireRight'
+    },
+    common: {
+       loading: 'Loading...',
+       error: 'Something went wrong',
+       retry: 'Try Again', 
+    },
+} as const;
